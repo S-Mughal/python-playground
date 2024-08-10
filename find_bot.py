70b1e9644fa40c0cb850a812854f58a7a597ec36
Saad Mughal <76440097+SMughal2020@users.noreply.github.com>
2024-08-10 00:13:55 -0400

    Create find_bot.py

---
+++ find_bot.py
@@ -0,0 +1,16 @@
+def find_boy(log: str, count: int) -> list[str]:
+  ip = []
+  bots = []
+  [ip.append(i.split(",")[1] for i in log]
+  ip_set = set(ip)
+  for i in ip_set:
+    if ip.count(i) >= count:
+      bots.append(i)
+  print(bots)
+
+logs = [
+  "1722024582, 52.0.1.1",
+  "1722024582, 52.0.1.1",
+  "1722024582, 52.0.1.1",
+  "1722024582, 52.0.1.1",
+]
